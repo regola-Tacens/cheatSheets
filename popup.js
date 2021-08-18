7a6894ea76305713a814ea2f9abb1bf319ab5e2d
regola-Tacens <regolas_tacens@outlook.fr>
2021-08-18 18:50:31 +0200

    ajout de méthode pour supprimer automatiquement la fenetre popup si on choisit une autre fenetre

--- popup.js
+++ popup.js
@@ -1,8 +1,30 @@
 
 let gitSheet = document.getElementById("git_Sheet");
+let CLI_Sheet =document.getElementById("CLI_Sheet");
+
+let currentWindow='';
 
 gitSheet.addEventListener("click", async () => {
+  // on retire la fenetre courante
+  await removeCurrentWindow();
 
-  chrome.windows.create({ url: chrome.runtime.getURL("popups/git_popup.html"), type: 
+  currentWindow = await chrome.windows.create({ url: chrome.runtime.getURL("popups/git_popup.html"), type: 
   "popup", height : 800, width : 500 });
+
+  chrome.storage.sync.set({currentWindow: currentWindow});
 });
+
+CLI_Sheet.addEventListener("click", async () => {
+  await removeCurrentWindow();
+  
+  currentWindow = await chrome.windows.create({ url: chrome.runtime.getURL("popups/terminal_popup.html"), type: 
+  "popup", height : 800, width : 500 });
+
+  chrome.storage.sync.set({currentWindow: currentWindow});
+});
+
+  const removeCurrentWindow = ()=> { 
+      chrome.storage.sync.get(['currentWindow'], function(result) {
+        currentWindow.id && chrome.windows.remove(result.currentWindow.id)
+      }); 
+  }
