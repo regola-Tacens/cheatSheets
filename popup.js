cd9a3486a8ee9e5c9f5c4b9a379266c72446559a
regola-Tacens <regolas_tacens@outlook.fr>
2021-08-19 07:14:24 +0200

    factorisation de fonctions EventListeners des boutons qu ouvrent les popup

--- popup.js
+++ popup.js
@@ -1,30 +1,33 @@
-
-let gitSheet = document.getElementById("git_Sheet");
-let CLI_Sheet =document.getElementById("CLI_Sheet");
+const sheets = document.querySelectorAll('button');
 
 let currentWindow='';
 
-gitSheet.addEventListener("click", async () => {
-  // on retire la fenetre courante
-  await removeCurrentWindow();
+for (const sheet of sheets){
+console.log(sheet.id)
+ sheet.addEventListener("click", async () => {
+    try {
+      // on efface le popup actif avant d'en créer un nouveau
+      await removeCurrentWindow();
 
-  currentWindow = await chrome.windows.create({ url: chrome.runtime.getURL("popups/git_popup.html"), type: 
-  "popup", height : 800, width : 500 });
+      // on crée le nouveau popup
+      currentWindow = await chrome.windows.create({ 
+        url: chrome.runtime.getURL(`popups/${sheet.id}.html`), 
+        type: "popup",
+        height : 800,
+         width : 500 });
+    
+      // on enregistre l'objet crée dans storage pour pouvoir récuperer son Id et l'effacer plus atrd
+      chrome.storage.sync.set({currentWindow: currentWindow});
+    } catch (error){
+      console.error(error)
+    }
+    
+  });
+}
 
-  chrome.storage.sync.set({currentWindow: currentWindow});
-});
-
-CLI_Sheet.addEventListener("click", async () => {
-  await removeCurrentWindow();
-  
-  currentWindow = await chrome.windows.create({ url: chrome.runtime.getURL("popups/terminal_popup.html"), type: 
-  "popup", height : 800, width : 500 });
-
-  chrome.storage.sync.set({currentWindow: currentWindow});
-});
-
-  const removeCurrentWindow = ()=> { 
-      chrome.storage.sync.get(['currentWindow'], function(result) {
-        currentWindow.id && chrome.windows.remove(result.currentWindow.id)
-      }); 
-  }
+// fonction qui se charge d'effacer le popup
+const removeCurrentWindow = ()=> { 
+  chrome.storage.sync.get(['currentWindow'], function(result) {
+    currentWindow.id && chrome.windows.remove(result.currentWindow.id)
+  }); 
+}
